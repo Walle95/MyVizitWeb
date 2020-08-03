194c4fe93d68ff5c018ac0dfd459457e99bda427
Walle95 <valentinshabanov95@gmail.com>
2020-08-03 22:39:27 +0700

    Сделан фильтор для поиска заметок

--- src/main/java/com/example/myvizitweb/GreetingController.java
+++ src/main/java/com/example/myvizitweb/GreetingController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -36,15 +37,27 @@ public class GreetingController {
     }
 
     @PostMapping
-    public String add (@RequestParam String text, @RequestParam String tag, Map<String,Object>model){
+    public String add (@RequestParam String text, @RequestParam String tag, Map<String, Object>model){
        Message message = new Message(text,tag);
-
        messageRepository.save(message);
 
         Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object>model){
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()){
+            messages = messageRepository.findByTag(filter);
+        }
+        else
+        {
+            messages = messageRepository.findAll();
+        }
 
         model.put("messages", messages);
-
         return "main";
     }
 
--- src/main/java/com/example/myvizitweb/repository/MessageRepository.java
+++ src/main/java/com/example/myvizitweb/repository/MessageRepository.java
@@ -3,5 +3,9 @@ package com.example.myvizitweb.repository;
 import com.example.myvizitweb.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
 }
